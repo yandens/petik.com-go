c137f7faa6acb04017a7a822b941d01ba292f238
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2023-07-23 21:46:52 +0800

    add default routes

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+  "github.com/gin-contrib/cors"
+  "github.com/gin-gonic/gin"
+  "github.com/yandens/petik.com-go/src/configs"
+)
 
 func main() {
-  fmt.Println("Hello World")
+  router := gin.Default()
+
+  router.Use(cors.New(cors.Config{
+    AllowOrigins: []string{"*"},
+    AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+    AllowHeaders: []string{"Origin", "Content-Type", "Authorization"},
+    MaxAge:       600,
+  }))
+
+  // routes
+  router.GET("/", func(c *gin.Context) {
+    c.JSON(200, gin.H{
+      "message": "Hello world!",
+    })
+  })
+
+  router.Run(configs.GetEnv("HOST") + ":" + configs.GetEnv("PORT"))
 }
