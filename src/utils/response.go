e8e5771994a97c26f6743581e15b494ef34366b0
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2023-07-24 21:20:30 +0800

    add json response

--- src/utils/response.go
+++ src/utils/response.go
@@ -1,1 +1,21 @@
 package utils
+
+import "github.com/gin-gonic/gin"
+
+type Response struct {
+  Status  int         `json:"status"`
+  Success bool        `json:"success"`
+  Message string      `json:"message"`
+  Data    interface{} `json:"data"`
+}
+
+func JSONResponse(c *gin.Context, status int, success bool, message string, data interface{}) {
+  response := Response{
+    Status:  status,
+    Success: success,
+    Message: message,
+    Data:    data,
+  }
+
+  c.JSON(status, response)
+}
