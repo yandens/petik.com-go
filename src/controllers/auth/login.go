9cb405ea925297ecfc4858ff9e48a1a40c889971
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2023-07-26 22:21:03 +0800

    add login controller

--- src/controllers/auth/login.go
+++ src/controllers/auth/login.go
@@ -17,24 +17,38 @@ func Login(c *gin.Context) {
   var input LoginInput
   var user models.User
 
+  // connect to database
   db, err := configs.ConnectToDB()
   if err != nil {
     utils.JSONResponse(c, 500, false, "Could not connect to the database", nil)
   }
 
+  // validate input
   if err := c.ShouldBindJSON(&input); err != nil {
     utils.JSONResponse(c, 400, false, "Input must be JSON", nil)
   }
 
+  // find user by email
   err = db.Where("email = ?", input.Email).First(&user).Error
   if err != nil {
     utils.JSONResponse(c, 500, false, "Incorrect Email or Password", nil)
   }
 
+  // compare password
   err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(input.Password))
   if err != nil && err == bcrypt.ErrMismatchedHashAndPassword {
     utils.JSONResponse(c, 400, false, "Incorrect Email or Password", nil)
   }
 
-  utils.JSONResponse(c, 200, true, "Success", nil)
+  // generate token
+  token, err := utils.GenerateToken(user.ID, user.Email, user.Role.Role)
+  if err != nil {
+    utils.JSONResponse(c, 500, false, "Could not generate token", nil)
+  }
+
+  // return response
+  utils.JSONResponse(c, 200, true, "Success", gin.H{
+    "email": user.Email,
+    "token": token,
+  })
 }
