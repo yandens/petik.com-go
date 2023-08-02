11ef4f98e021a08c7d95e4892aa01d02d112633c
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2023-08-02 17:06:49 +0800

    update field name

--- src/models/user.go
+++ src/models/user.go
@@ -4,9 +4,9 @@ import "gorm.io/gorm"
 
 type User struct {
   gorm.Model
-  Role      Role   `gorm:"foreignKey:RoleID"`
-  RoleID    uint   `gorm:"not null"`
-  Email     string `gorm:"unique;not null"`
-  Password  string `gorm:"not null"`
-  IsVerifed bool   `gorm:"default:false"`
+  Role       Role   `gorm:"foreignKey:RoleID"`
+  RoleID     uint   `gorm:"not null"`
+  Email      string `gorm:"unique;not null"`
+  Password   string `gorm:"not null"`
+  IsVerified bool   `gorm:"default:false"`
 }
