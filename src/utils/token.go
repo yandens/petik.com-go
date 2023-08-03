8136180d26bbdc3059d516a05beeb2962759528f
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2023-08-03 18:19:12 +0800

    add token claims function and encrypt token payload

--- src/utils/token.go
+++ src/utils/token.go
@@ -4,14 +4,30 @@ import (
   "fmt"
   "github.com/golang-jwt/jwt/v5"
   "github.com/yandens/petik.com-go/src/configs"
+  "strconv"
   "time"
 )
 
 func GenerateToken(userID uint, email string, role string) (string, error) {
+  encryptedID, err := Encrypt([]byte(strconv.Itoa(int(userID))))
+  if err != nil {
+    return "", err
+  }
+
+  encryptedEmail, err := Encrypt([]byte(email))
+  if err != nil {
+    return "", err
+  }
+
+  encryptedRole, err := Encrypt([]byte(role))
+  if err != nil {
+    return "", err
+  }
+
   token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-    "id":    userID,
-    "email": email,
-    "role":  role,
+    "id":    encryptedID,
+    "email": encryptedEmail,
+    "role":  encryptedRole,
     "exp":   time.Now().Add(time.Hour * 24).Unix(),
   })
 
@@ -39,4 +55,36 @@ func ValidateToken(tokenString string) (*jwt.Token, error) {
   }
 
   return token, nil
+}
+
+func TokenClaims(token *jwt.Token) (jwt.MapClaims, error) {
+  claims, ok := token.Claims.(jwt.MapClaims)
+  if !ok {
+    return nil, fmt.Errorf("Invalid token")
+  }
+
+  // decrypt user id
+  decryptedUserID, err := Decrypt([]byte(claims["id"].(string)))
+  if err != nil {
+    return nil, err
+  }
+
+  // decrypt user email
+  decryptedEmail, err := Decrypt([]byte(claims["email"].(string)))
+  if err != nil {
+    return nil, err
+  }
+
+  // decrypt user role
+  decryptedRole, err := Decrypt([]byte(claims["role"].(string)))
+  if err != nil {
+    return nil, err
+  }
+
+  // set claims
+  claims["id"] = decryptedUserID
+  claims["email"] = decryptedEmail
+  claims["role"] = decryptedRole
+
+  return claims, nil
 }
