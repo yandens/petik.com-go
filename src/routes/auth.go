e4702c75803d04813fe5a86b2115b1dc02923843
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2023-08-13 20:34:54 +0800

    add change password route and controller

--- src/routes/auth.go
+++ src/routes/auth.go
@@ -13,6 +13,7 @@ func AuthRoutes(router *gin.RouterGroup) {
   authRoute.GET("/verify-email", auth.VerifyEmail)
   authRoute.POST("/forgot-password", auth.ForgotPassword)
   authRoute.PUT("/reset-password", auth.ResetPassword)
+  authRoute.PUT("/change-password", middlewares.Authorized("user", "admin"), auth.ChangePassword)
   authRoute.GET("/whoami", middlewares.Authorized("user", "admin"), auth.WhoAmI)
   authRoute.GET("/google", auth.GoogleOauth2)
   authRoute.GET("/google/callback", auth.GoogleOauth2Callback)
