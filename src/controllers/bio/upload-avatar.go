6eb57f875beea9253b83a21a9309a9e84845cc6a
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2023-08-30 18:44:20 +0800

    rename folder name

--- src/controllers/bio/upload-avatar.go
+++ src/controllers/bio/upload-avatar.go
@@ -1,4 +1,4 @@
-package user
+package bio
 
 import (
   "github.com/gin-gonic/gin"
--- src/routes/user.go
+++ src/routes/user.go
@@ -2,16 +2,16 @@ package routes
 
 import (
   "github.com/gin-gonic/gin"
-  "github.com/yandens/petik.com-go/src/controllers/user"
+  "github.com/yandens/petik.com-go/src/controllers/bio"
   "github.com/yandens/petik.com-go/src/middlewares"
 )
 
-func UserRoutes(router *gin.RouterGroup) {
-  userRoute := router.Group("/user")
-  userRoute.POST("/create-bio", middlewares.Authorized("user"), user.CreateBio)
-  userRoute.PUT("/update-bio", middlewares.Authorized("user"), user.UpdateBio)
-  userRoute.GET("/get-bio", middlewares.Authorized("user"), user.ReadBio)
-  userRoute.POST("upload-avatar", middlewares.Authorized("user"), user.UploadAvatar)
-  userRoute.GET("/get-users", middlewares.Authorized("admin"), user.GetUsers)
-  userRoute.GET("/get-user/:id", middlewares.Authorized("admin"), user.GetUser)
+func BioRoutes(router *gin.RouterGroup) {
+  bioRoute := router.Group("/bio")
+  bioRoute.POST("/create-bio", middlewares.Authorized("user"), bio.CreateBio)
+  bioRoute.PUT("/update-bio", middlewares.Authorized("user"), bio.UpdateBio)
+  bioRoute.GET("/get-bio", middlewares.Authorized("user"), bio.ReadBio)
+  bioRoute.POST("upload-avatar", middlewares.Authorized("user"), bio.UploadAvatar)
+  bioRoute.GET("/get-users", middlewares.Authorized("admin"), bio.GetUsers)
+  bioRoute.GET("/get-user/:id", middlewares.Authorized("admin"), bio.GetUser)
 }
