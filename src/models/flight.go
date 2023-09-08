47389e2fbe2a1c21eacb2b99c9c46ffb89a967d1
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2023-09-08 11:22:11 +0800

    add flight model

---
+++ src/models/flight.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+  "gorm.io/gorm"
+  "time"
+)
+
+type Flight struct {
+  gorm.Model
+  Airline         string    `gorm:"not null"`
+  AirlineLogo     string    `gorm:"not null"`
+  Origin          string    `gorm:"not null"`
+  OriginCity      string    `gorm:"not null"`
+  Destination     string    `gorm:"not null"`
+  DestinationCity string    `gorm:"not null"`
+  Departure       time.Time `gorm:"not null"`
+  Arrival         time.Time `gorm:"not null"`
+}
