9a6316f4b9ff8ad622ca1919837bb7f2f0c9ae0c
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2023-10-13 11:19:48 +0800

    add comment in GetEnv function and update ingress.yaml

--- src/configs/env.go
+++ src/configs/env.go
@@ -5,7 +5,10 @@ import (
 )
 
 func GetEnv(env string) string {
+  // use local .env file
   //err := godotenv.Load("../.env")
+
+  // use kubernetes secret (mounted as volume)
   byte, err := ioutil.ReadFile("/app/.env/" + env)
   if err != nil {
     panic(err)
